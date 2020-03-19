5f7947eb570fd226bf32fd4639ad4309dd4a93ac
Monalisa Das <monalisa.svnit@gmail.com>
2020-03-19 10:14:36 +0000

    Committing seed project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello, This is React Starter App. Change your code here to get started
+      </div>
+    );
+  }
+}
+
+export default App;
